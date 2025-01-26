0e95d9a5336d4706738582b22d70d5d468fffc12
merkelmarrow <69924036+merkelmarrow@users.noreply.github.com>
2025-01-26 00:25:33 +0000

    fix: corrected padding calculation logic

--- learn_cpp_bitmap_2025/learn_cpp_bitmap_2025.cpp
+++ learn_cpp_bitmap_2025/learn_cpp_bitmap_2025.cpp
@@ -12,8 +12,8 @@ private:
 
 public:
 	PixArr(uint32_t image_width, uint32_t image_height) {
-		uint32_t total_width;
-		for (total_width = 1; total_width < image_width; total_width *= 2) {}
+		uint32_t total_width{ image_width % 4 == 0 ? 
+			image_width : image_width - (image_width % 4) + 4};
 		m_padding = static_cast<uint16_t>(total_width - image_width);
 		m_pix_arr = std::make_unique<uint16_t[]>((total_width / 8) * image_height);
 	}
@@ -23,7 +23,7 @@ class Bitmap {
 private:
 	const char* sig{ "BM" };
 
-	// File header
+	// File headers
 	uint32_t m_file_size;
 	uint16_t m_reserved_1;
 	uint16_t m_reserved_2;
@@ -46,27 +46,26 @@ private:
 	std::unique_ptr<uint16_t> m_palette;
 
 	// Pixel Array
+	PixArr m_pix_arr;
 
 public:
 	Bitmap(uint32_t image_width, uint32_t image_height) :
 		m_image_width(image_width), m_image_height(image_height), m_colour_planes(1),
 		m_bits_per_pix(8), m_compress_type(0), m_horizontal_res(20), m_vertical_res(20),
-		m_dib_header_size(40), m_pallete_colours(0), m_important_colours(0) {
-		// Need to allocate m_pix_arr
-		// Need to find:
+		m_dib_header_size(40), m_pallete_colours(0), m_important_colours(0),
+		m_pix_arr(PixArr(m_image_width, m_image_height)) {
+
+		// TODO: Need to find:
 			// m_file_size
 			// m_pix_arr_offset
 			// m_image_size
-		uint16_t padding
-		for (size_t i = 1; i < m_image_width; i *= 2) {}
-
 	}
 };
 
 
 int main()
 {
-	
+
 }
 
 
